5bcb2147f5f29e754434316e79deb9126bdee0f4
danivelop <mofg266@gmail.com>
2021-10-31 23:54:22 +0900

    road fetching로직 구현

--- src/pages/edit/[id].tsx
+++ src/pages/edit/[id].tsx
@@ -1,7 +1,26 @@
+/* External dependencies */
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+/* Internal dependencies */
+import { requestGetRoad } from 'stores/actions/roadActions';
+import { setRoad } from 'stores/actions/editActions';
+import { getRoad } from 'stores/selectors/roadSelectors';
 import Layout from 'components/Layout';
 import MakeRoad from 'components/templates/MakeRoad';
 
 function Edit() {
+  const dispatch = useDispatch();
+  const road = useSelector(getRoad);
+
+  useEffect(() => {
+    dispatch(requestGetRoad({ roadId: '142' }));
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(setRoad({ road }));
+  }, [dispatch, road]);
+
   return (
     <Layout>
       <MakeRoad />
--- src/stores/selectors/roadSelectors.ts
+++ src/stores/selectors/roadSelectors.ts
@@ -4,3 +4,5 @@ import { RootState } from 'stores/reducers';
 export const getTest = (state: RootState) => state.roadReducer.test;
 
 export const getImgDetail = (state: RootState) => state.roadReducer.imgDetail;
+
+export const getRoad = (state: RootState) => state.roadReducer.road;

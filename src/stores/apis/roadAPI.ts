da50133c13205006c4e0300dbc1efbed803fdc45
danivelop <mofg266@gmail.com>
2021-11-14 01:41:20 +0900

    임시로 인증 토큰 추가

--- src/stores/apis/roadAPI.ts
+++ src/stores/apis/roadAPI.ts
@@ -6,5 +6,10 @@ import { RequestGetRoadPayload } from 'stores/actions/roadActions';
 import { getEndpoint } from 'utils/requestUtils';
 
 export const getRoad = ({ roadId }: RequestGetRoadPayload) => {
-  return axios.get(`${getEndpoint()}/v1/apis/roads/${roadId}`);
+  return axios.get(`${getEndpoint()}/v1/apis/roads/${roadId}`, {
+    headers: {
+      authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQG5hdmVyLmNvbSIsInN1YiI6IjgiLCJpYXQiOjE2MzY4MjEzNDcsImV4cCI6MTYzOTQxMzM0N30.jQPZ2hZ27ka3QSeM2BJVFFk80_f6WE1rjT0u69vd5hY',
+    },
+  });
 };

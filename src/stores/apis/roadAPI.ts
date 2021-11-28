b7001b45049c5c748db9ca5898515628bb41481e
danivelop <mofg266@gmail.com>
2021-11-28 23:31:07 +0900

    길삭제 api에 토큰 추가

--- src/stores/apis/roadAPI.ts
+++ src/stores/apis/roadAPI.ts
@@ -19,6 +19,9 @@ export const getRoad = ({ roadId }: RequestGetRoadPayload) => {
 
 export const removeRoad = ({ roadId }: RequestRemoveRoadPayload) => {
   return axios.delete(`${getEndpoint()}/v1/apis/roads/${roadId}`, {
-    headers: {},
+    headers: {
+      authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQG5hdmVyLmNvbSIsInN1YiI6IjgiLCJpYXQiOjE2MzY4MjEzNDcsImV4cCI6MTYzOTQxMzM0N30.jQPZ2hZ27ka3QSeM2BJVFFk80_f6WE1rjT0u69vd5hY',
+    },
   });
 };
